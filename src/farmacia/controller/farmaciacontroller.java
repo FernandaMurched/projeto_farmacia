0f29aa66a7c38a84f6404f39d81d39afe19fd8ec
Fernanda Murched <fernanda_hco@outlook.com.br>
2025-05-06 23:03:59 -0300

    Corrige alterações não incluídas no commit anterior

--- src/farmacia/controller/farmaciacontroller.java
+++ src/farmacia/controller/farmaciacontroller.java
@@ -1,5 +1,43 @@
 package farmacia.controller;
 
-public class farmaciacontroller {
+import java.util.ArrayList;
+
+import farmacia.model.Produto;
+import farmacia.repository.farmaciarepository;
+
+public class farmaciacontroller implements farmaciarepository {
+
+	private ArrayList<Produto> produtosCadastrados = new ArrayList<Produto>();
+	int id = 0;
+	
+	@Override
+	public void criarProduto(Produto produto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarTodosProdutos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void consultarProdutoPorId(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizarProduto(Produto produto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean deletarProduto(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- src/farmacia/repository/farmaciarepository.java
+++ src/farmacia/repository/farmaciarepository.java
@@ -1,5 +1,12 @@
 package farmacia.repository;
 
+import farmacia.model.Produto;
+
 public interface farmaciarepository {
 
+	public void criarProduto(Produto produto);
+	public void listarTodosProdutos();
+	public void consultarProdutoPorId(int id);
+	public void atualizarProduto(Produto produto);
+	public boolean deletarProduto(int id);
 }
